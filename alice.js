b498b90be5c914294ebbeed6a28f8ab8521442b9
tomual <tomual@users.noreply.github.com>
2020-07-01 22:51:56 +0800

    Fetch messages from given channel ID

--- alice.js
+++ alice.js
@@ -9,6 +9,8 @@ client.on('ready', () => {
     printGuild(guild);
     printChannels(guild);
     printMembers(guild);
+
+    getMessages(guild.channels.cache.get('724442665093038110'));
 });
 
 function getGuild(client) {
@@ -37,4 +39,25 @@ function printMembers(guild)
     for (var member of guild.members.cache) {
         console.log(member[1].user.id + "\t" + member[1].user.username + "#" + member[1].user.discriminator);
     }
+}
+
+var messagesArray = [];
+function getMessages(channel, beforeMessageId = null) {
+    channel.messages.fetch({ limit: 100, before: beforeMessageId })
+    .then(messages => {
+        var cursorMessageId = null;
+        for (var message of messages) {
+            console.log(message); 
+            messagesArray.push(message[1].content);
+            cursorMessageId = message[1].id;
+        }
+        console.log(messages.size);
+        if (messages.size > 0 && cursorMessageId != beforeMessageId) {
+            console.log("next page - " + cursorMessageId);
+            setTimeout(() => {getMessages(channel, cursorMessageId)}, 1000);
+        } else {
+            console.log(JSON.stringify(messagesArray)); 
+        }
+    })
+    .catch(console.error);
 }

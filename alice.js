8bb840ee7e6926c9c229f743366066fa4e51fd4a
tomual <tomual@users.noreply.github.com>
2020-07-04 17:39:11 +0800

    Listen for reactions

--- alice.js
+++ alice.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const auth = require('./auth.json');
 
 client.login(auth.token);
 
 client.on('ready', () => {
     var guild = getGuild(client);
+
     printGuild(guild);
-    console.log(guild);
     printEmojis(guild);
     printChannels(guild);
     printMembers(guild);
@@ -20,6 +20,20 @@ client.on('message', message => {
     message.content = message.content.toLowerCase();
 
     postReaction(message);
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.log('Something went wrong when fetching the message: ', error);
+            return;
+        }
+    }
+    if (reaction._emoji.name == "💯") {
+        console.log(user.username + " reacted to Message ID:" + reaction.message.id + " with 💯!");
+    }
 });
 
 function postReaction(message) {
